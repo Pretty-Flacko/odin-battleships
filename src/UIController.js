330751395c229eb80097119711b2ae2b3cde24e7
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-04-11 01:55:39 +0400

    Create UIController class

---
+++ src/UIController.js
@@ -0,0 +1,51 @@
+export default class UI {
+	constructor(game) {
+		this.game = game;
+
+		this.playerBoardEl = document.querySelector("#player-board");
+		this.enemyBoardEl = document.querySelector("#enemy-board");
+	}
+
+	renderBoard(board, container, isEnemy = false) {
+		container.innerHTML = "";
+
+		board.grid.forEach((row, y) => {
+			row.forEach((cell, x) => {
+				const div = document.createElement("div");
+				div.classList.add("cell");
+
+				if (cell.ship && !isEnemy) {
+					div.classList.add("ship");
+				}
+
+				div.dataset.x = x;
+				div.dataset.y = y;
+
+				container.appendChild(div);
+			});
+		});
+	}
+
+	addEnemyBoardListeners() {
+		this.enemyBoardEl.addEventListener("click", (e) => {
+			const cell = e.target;
+			if (!cell.classList.contains("cell")) return;
+
+			const x = Number(cell.dataset.x);
+			const y = Number(cell.dataset.y);
+
+			const result = this.game.playTurn(x, y);
+
+			this.update();
+
+			if (result.winner) {
+				alert(`${result.winner.type} wins!`);
+			}
+		});
+	}
+
+	update() {
+		this.renderBoard(this.game.player1.board, this.playerBoardEl, false);
+		this.renderBoard(this.game.player2.board, this.enemyBoardEl, true);
+	}
+}
